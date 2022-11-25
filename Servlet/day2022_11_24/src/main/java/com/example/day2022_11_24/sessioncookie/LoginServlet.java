95fe3949ee89821175ecea7ff97fcf87638d1131
admin <247412755@qq.com>
2022-11-25 16:03:50 +0800

    2022_11_25测试提交

--- Servlet/day2022_11_24/src/main/java/com/example/day2022_11_24/sessioncookie/LoginServlet.java
+++ Servlet/day2022_11_24/src/main/java/com/example/day2022_11_24/sessioncookie/LoginServlet.java
@@ -1,6 +1,5 @@
 package com.example.day2022_11_24.sessioncookie;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,24 +18,16 @@ public class LoginServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         PrintWriter writer = resp.getWriter();
-
-
         if (! username.equals("admin") && !password.equals("admin")){
             writer.write("账号密码输入错误");
             return;
         }
         writer.write("登录成功");
-
-
         HttpSession httpSession = req.getSession();
         httpSession.setAttribute("username",username);
         httpSession.setAttribute("password",password);
         httpSession.setMaxInactiveInterval(10);
         httpSession.setAttribute("loginCount",0);
         resp.sendRedirect("index");
-
-
-
-
     }
 }
